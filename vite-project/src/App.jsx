3628bcd6c09dff8493f1174c777293c0a7912fc5
boben195 <boben195@ukr.net>
2025-02-01 19:40:38 +0200

    play with small tasks-one

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,20 @@
 // import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+// import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <img src={reactLogo} />
+      <div className="container">
+        <img
+          className="header-img"
+          src="./vite-project/src/assets/icons8-google-scholar-50.png"
+          alt=""
+        />
+        <h1>my travel jornal</h1>
+      </div>
+      {/* <img src={reactLogo} />
       <h1>Fun facts about React</h1>
       <ul>
         <li>Was first released in 2013</li>
@@ -15,27 +23,7 @@ function App() {
         <li>Is maintained by META</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
         <li>test</li>
-      </ul>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      </ul> */}
     </>
   );
 }
