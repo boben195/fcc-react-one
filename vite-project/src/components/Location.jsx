dbc5db965a9dfdb843c5f8accd0772c9f35b0964
boben195 <boben195@ukr.net>
2025-02-06 23:02:41 +0200

    six

--- vite-project/src/components/Location.jsx
+++ vite-project/src/components/Location.jsx
@@ -3,13 +3,6 @@ import "/src/modules/location.css";
 const Location = () => {
   return (
     <div className="section-container">
-      {/* <article className="lorny">
-        <div className="main-image-container">
-          <img className="main-image" src="/public/svg/japan.jpg" alt="" />
-        </div>
-        <span>Japan</span>
-        <a href="https://maps.app.goo.gl/KdjQLRzWFwArXPzB8">View on Google</a>
-      </article> */}
       <img className="japan-img" src="/public/svg/japan.jpg" alt="japan" />
       <div className="location-container">
         <img
@@ -26,12 +19,6 @@ const Location = () => {
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia cum sit
           eius pariatur sequi sint?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          cupiditate suscipit culpa dicta officiis sunt asperiores dolor totam
-          quia, qui rem officia eveniet eum itaque nesciunt fuga eos incidunt!
-          Suscipit.
         </p>
       </div>
     </div>
